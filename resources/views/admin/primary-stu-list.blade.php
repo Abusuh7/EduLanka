5e75645307b48729997ed4371cc47f3019598394
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-08 17:40:33 +0530

    UserManagement-5

--- resources/views/admin/primary-stu-list.blade.php
+++ resources/views/admin/primary-stu-list.blade.php
@@ -8,37 +8,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 border-4">
-
             <ul role="list" class="divide-y divide-gray-100">
-
-
-                    {{-- @foreach ($teachers as $teacher)
+                @if (count($primaryStudents) === 0)
+                    <p class="text-center text-lg text-gray-500 py-4">No primary students enrolled.</p>
+                @else
+                @foreach ($primaryStudents as $primaryStudent)
+                        <!-- Your existing teacher display code here -->
                         <li class="flex justify-between gap-x-6 py-5">
                             <div class="flex min-w-0 gap-x-4">
                                 <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="storage/app/public/{{ $teacher->profile_photo_path }}"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">{{ $teacher->name }}</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $teacher->email }}</p>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">{{ ucwords($teacher->role) }}</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h
-                                        ago</time></p>
-                            </div>
-                        </li>
-                    @endforeach --}}
-
-                    @foreach ($primaryStudents as $primaryStudent)
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex min-w-0 gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="storage/app/public/{{ $primaryStudent->profile_photo_path }}"
-                                    alt="">
+                                    src="storage/app/public/{{ $primaryStudent->profile_photo_path }}" alt="">
                                 <div class="min-w-0 flex-auto">
                                     <p class="text-sm font-semibold leading-6 text-gray-900">{{ $primaryStudent->name }}</p>
                                     <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $primaryStudent->email }}</p>
@@ -53,34 +32,10 @@
                             </div>
                         </li>
                     @endforeach
-
-                    {{-- @foreach ($secondaryStudents as $secondaryStudent)
-                        <li class="flex justify-between gap-x-6 py-5">
-                            <div class="flex min-w-0 gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="storage/app/public/{{ $secondaryStudent->profile_photo_path }}"
-                                    alt="">
-                                <div class="min-w-0 flex-auto">
-                                    <p class="text-sm font-semibold leading-6 text-gray-900">{{ $secondaryStudent->name }}</p>
-                                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $secondaryStudent->email }}</p>
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                <p class="text-sm leading-6 text-gray-900">{{ ucwords($secondaryStudent->role) }}</p>
-                                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time
-                                        datetime="2023-01-23T13:23Z">3h
-                                        ago</time></p>
-                            </div>
-                        </li>
-                    @endforeach --}}
-
-
-
+                @endif
             </ul>
         </div>
     </div>
-
 
 
 </x-app-layout>
