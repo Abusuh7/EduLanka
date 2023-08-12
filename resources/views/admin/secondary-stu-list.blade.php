457631f840ba9cf60d38fa87c91e79c9770db9e6
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-12 06:15:40 +0530

    User-Management-8

--- resources/views/admin/secondary-stu-list.blade.php
+++ resources/views/admin/secondary-stu-list.blade.php
@@ -17,7 +17,7 @@
                         <li class="flex justify-between gap-x-6 py-5">
                             <div class="flex min-w-0 gap-x-4">
                                 <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                    src="storage/app/public/{{ $secondaryStudent->profile_photo_path }}" alt="">
+                                    src="/storage/{{ $primaryStudent->profile_photo_path }}" alt="">
                                 <div class="min-w-0 flex-auto">
                                     <p class="text-sm font-semibold leading-6 text-gray-900">{{ $secondaryStudent->name }}</p>
                                     <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $secondaryStudent->email }}</p>
