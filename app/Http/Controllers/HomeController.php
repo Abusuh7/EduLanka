fa787dcc34cef0b9ce878de1405323c070fb4fb7
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-17 23:16:30 +0530

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,19 +12,30 @@ class HomeController extends Controller
     {
         $role = Auth::user()->role;
 
-        if($role == 'admin'){
-            return view('admin.dashboard');
-        }
-        elseif($role == 'teacher')
-        {
+        if ($role == 'admin') {
+            // Get all the primary students count
+            $students = User::where('role', 'primary')->get();
+            $primaryStudentCount = $students->count();
+
+            // get all the secondary students count
+            $secondaryStudents = User::where('role', 'secondary')->get();
+            $secondaryStudentCount = $secondaryStudents->count();
+
+            // Get all the teachers count
+            $teachers = User::where('role', 'teacher')->get();
+            $teacherCount = $teachers->count();
+
+            // Get the total number of primary and secondary students
+            $totalStudents = $primaryStudentCount + $secondaryStudentCount;
+
+
+            return view('admin.dashboard', compact('primaryStudentCount', 'secondaryStudentCount', 'teacherCount', 'totalStudents'));
+            // return view('admin.dashboard');
+        } elseif ($role == 'teacher') {
             return view('teacher.dashboard');
-        }
-        elseif($role == 'primary')
-        {
+        } elseif ($role == 'primary') {
             return view('primaryStudent.dashboard');
-        }
-        else
-        {
+        } else {
             return view('secondaryStudent.dashboard');
         }
     }
