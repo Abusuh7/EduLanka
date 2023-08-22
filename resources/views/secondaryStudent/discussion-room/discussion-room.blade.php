2c8ac24b01858304516ffd4d1d5bbc3d55a96c40
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-22 10:59:24 +0530

    Discussion Room 4

--- resources/views/secondaryStudent/discussion-room/discussion-room.blade.php
+++ resources/views/secondaryStudent/discussion-room/discussion-room.blade.php
@@ -6,6 +6,19 @@
     </x-slot>
 
 
+    @if ($errors->any())
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </div>
+    @endif
+
+    @if (session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            {{ session('success') }}
+        </div>
+    @endif
 
     <div class="grid grid-cols-3 gap-4">
         {{-- First Card --}}
@@ -108,13 +121,7 @@
         {{-- ... (additional cards) ... --}}
     </div>
 
-    <div>
-        <label for="time_slot">Status</label>
-        <input type="text" name="time_slot" id="time_slot">
-        @error('time_slot')
-            <p class="text-red-500">{{ $message }}</p>
-        @enderror
-    </div>
+
 
 
 
