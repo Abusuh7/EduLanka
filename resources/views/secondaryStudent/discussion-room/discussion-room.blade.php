e752d8866e734aada45e1c3af369f2185ca15e85
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-22 16:50:28 +0530

    Discussion Room 6

--- resources/views/secondaryStudent/discussion-room/discussion-room.blade.php
+++ resources/views/secondaryStudent/discussion-room/discussion-room.blade.php
@@ -60,7 +60,7 @@
                     Availability: <span class="text-green-500">Available</span>
                 </p>
                 <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    Status: <span class="text-green-500">{{ $all->status }}</span>
+                    Status: <span class="text-green-500">Display Status</span>
                 </p>
                 <button type="button" id="discussionRoom1Btn"
                     class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
