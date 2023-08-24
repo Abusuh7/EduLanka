67c1a10a4f7cb239c1573ca14a9ceccda1116d1a
Abdullah Suhail <107505201+Abusuh7@users.noreply.github.com>
2023-08-25 00:38:20 +0530

    Discussion Room 10

--- resources/views/secondaryStudent/reservations/reservations-dashboard.blade.php
+++ resources/views/secondaryStudent/reservations/reservations-dashboard.blade.php
@@ -53,7 +53,6 @@
 
     {{-- Today Booking Table --}}
     <div class="py-12 hidden" id="todayBookingTable">
-        <h3>Today</h3>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
