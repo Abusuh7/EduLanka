bae8fb3f6f7e073ac6e8adbf322b45da8d9f5556
Kaleel Aadil <cb010188@students.apiit.lk>
2023-10-07 23:21:07 +0530

    added the nav directions by role

---
+++ vendor/laravel/jetstream/src/Http/Livewire/NavigationMenu.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Laravel\Jetstream\Http\Livewire;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class NavigationMenu extends Component
+{
+    /**
+     * The component's listeners.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'refresh-navigation-menu' => '$refresh',
+    ];
+
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+//        edited by me
+        $role = Auth::user()->role;
+        $status = Auth::user()->status;
+
+        if($role == 'admin'){
+            return view('admin.navigation-menu');
+        }
+        elseif($role == 'teacher' && $status == 'activated')
+        {
+            return view('teacher.navigation-menu');
+        }
+        elseif($role == 'primary' && $status == 'activated')
+        {
+            return view('primaryStudent.navigation-menu');
+        }
+        elseif($role == 'secondary' && $status == 'activated')
+        {
+            return view('secondaryStudent.navigation-menu');
+        }
+        elseif($role == 'teacher' && $status == 'deactivated' || $role == 'primary' && $status == 'deactivated' || $role == 'secondary' && $status == 'deactivated')
+        {
+            return view('deactivated-navigation-menu');
+        }else{
+            return view('terminated-navigation-menu');
+        }
+    }
+}
